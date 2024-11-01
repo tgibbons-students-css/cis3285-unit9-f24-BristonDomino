bc75591dd15eeef882097e6bbf5f405562d98a4c
Briston Domino <Bdomino1@css.edu>
2024-10-31 19:54:45 -0500

    Request 405 - "As a manager, I want to easily monitor the trading errors my traders make so I can provide them better training to avoid errors." Done part 4

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -6,14 +6,31 @@ namespace SingleResponsibilityPrinciple
 {
     public class ConsoleLogger : ILogger
     {
-        public void LogWarning(string message, params object[] args)
+        public void LogMessage(string type, string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("WARN: ", message), args);
+            // Output to console
+            Console.WriteLine(type + ": " + message, args);
+
+            // Append log to XML file
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>{0}</type><message>{1}</message></log>", type, string.Format(message, args));
+            }
         }
 
         public void LogInfo(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("INFO: ", message), args);
+            LogMessage("INFO", message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            LogMessage("WARN", message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            LogMessage("ERROR", message, args);
         }
     }
 }
